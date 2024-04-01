746e346251509aeb34a6b8d62a029996ad022054
isro24 <isro.usman.ft22@mail.umy.ac.id>
2024-04-01 16:17:35 +0700

    membuat io dan juga menambahkan looing for untuk membuat data sampai 3 data

--- struct4.cpp
+++ struct4.cpp
@@ -13,3 +13,24 @@ struct Mahasiswa{
     DetailAlamat alamat;
     int umur;
 };
+
+int main (){
+    Mahasiswa mhs [3];
+    for (int i=0;i<3;i++){
+        cout << "Data ke- " <<(i+1)<< ":" << endl;
+        cout << "Nomor Induk Mahasiswa : ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
+
+        cout << "Alamat : " << endl;
+        cout << "\t Nama Kota : ";
+        cin >> mhs[i].alamat.kota;
+        cout << "\t Nama Desa : ";
+        cin >> mhs[i].alamat.desa;
+    
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+        cout << endl;
+    }
